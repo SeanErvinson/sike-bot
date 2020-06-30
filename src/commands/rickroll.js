039fc8477bfc8457354ea06d4c1c7601fcca43e9
Sean Ervinson <ervinsonong@gmail.com>
2020-06-30 18:50:45 +0800

    feat: implemented re-roll functionality

--- src/commands/rickroll.js
+++ src/commands/rickroll.js
@@ -3,13 +3,22 @@ module.exports = {
     args: false,
     description: 'The re-roll the previous entries',
     execute(message, args) {
-        const fs = require('fs');
         const path = require('path');
         const location = path.join(__dirname, '..', 'assets', 'wheel.json');
-        if (fs.existsSync(location)) {
-            fs.readFileSync(location);
+        const spin = require('../utils/spin');
+        const file = require('../utils/file');
+        const prettyPrint = require('../utils/prettyprint');
+
+        const data = file.load(location);
+        const jsonData = JSON.parse(data);
+        const entries = jsonData['entries'];
+
+        if (entries.length > 0) {
+            const response = prettyPrint.format(entries);
+            message.channel.send(`Re-rolling \n${response}`);
+            spin.spin(message, entries);
         } else {
-            message.reply('No previous wheel. Execute `!status` to check wheel entries.');
+            message.reply('No previous wheel. Use `!stw` to start a new wheel.');
         }
     },
 };
